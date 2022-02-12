913d162ff01e69ebb24e888829c9fb74f629e620
mohsen mokhtari <mr.marsxan@gmail.com>
2022-02-12 16:57:35 +0330

    Initial Commit

---
+++ KiliaroTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KiliaroTest
+//
+//  Created by mohsen mokhtari on 2/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ KiliaroTest/KiliaroTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  KiliaroTestApp.swift
+//  KiliaroTest
+//
+//  Created by mohsen mokhtari on 2/12/22.
+//
+
+import SwiftUI
+
+@main
+struct KiliaroTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
